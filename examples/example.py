98b8a1f6d7cb3c9e117203c52c547b1985fa1b21
Ed <edfillingham1@gmail.com>
2023-12-18 20:05:51 +0000

    Created distribution and uploaded to pypi

--- examples/example.py
+++ examples/example.py
@@ -12,25 +12,25 @@ import gradient_descent.points as gd_points
 import matplotlib.pyplot as plt
 
 # Create points following a line 2x^2 -3x +4
-correct_coefficients = [2,-3,4]
+correct_coefficients = [2, -3, 4]
 points_array = gd_points.create_points(correct_coefficients, point_count=200, offset_range=0.3)
 
 # Do a train test split and print the results
-train_set, test_set = gd_points.train_test_split(points_array,test_size=0.4)
+train_set, test_set = gd_points.train_test_split(points_array, test_size=0.4)
 
 print(f'Training Size = {len(train_set)}')
 print(f'Testing Size = {len(test_set)}')
 
 # Train the model to get a line with estimated parameters
-trained_coefficients = gd.gradient_descent(3, debug = False)
+trained_coefficients = gd.gradient_descent(3, points_array, debug=False)
 print(f'Trained Coefficients = {trained_coefficients}')
 
 # Draw graphs to display the data
-gd_points.get_scatter_points(points_array, plt, color='y') # Plot All The point data on the graph in yellow
-gd_points.draw_line(correct_coefficients, plt, color = 'r') # Plot a red line of the correct curve
-gd_points.draw_line(trained_coefficients, plt, color='b') # Plot a blur line of the trained curve
+gd_points.get_scatter_points(points_array, plt, color='y')  # Plot All The point data on the graph in yellow
+gd_points.draw_line(correct_coefficients, plt, color='r')  # Plot a red line of the correct curve
+gd_points.draw_line(trained_coefficients, plt, color='b')  # Plot a blur line of the trained curve
 
 # display the score of the model
-print(f'Score is {gd.score(test_set,trained_coefficients)}')
+print(f'Score is {gd.score(test_set, trained_coefficients)}')
 
 plt.show()
